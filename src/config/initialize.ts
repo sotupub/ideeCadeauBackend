c73d93b58a892eae2ab7b60f0042419082dc7cf2
Mohamed <mohamedbenameur19@gmail.com>
2024-08-24 15:05:37 +0100

    add consoles

--- src/config/initialize.ts
+++ src/config/initialize.ts
@@ -8,10 +8,13 @@ import { encrypt } from '../helpers/helpers';
 
 export const test = async () => {
   try {
+    console.log("entered");
+    
     const userRepository = AppDataSource.getRepository(User);
     
     const existingAdmin = await userRepository.count({ where: { role: ERole.ADMIN } });
-
+    console.log(existingAdmin);
+    
     // Check if there are less than 1 admin, create the default ones
     if (existingAdmin < 1) {
       const encryptedPassword = await encrypt.encryptpass("admin123");
@@ -24,7 +27,10 @@ export const test = async () => {
         user.role = ERole.ADMIN;
     
         await userRepository.save(user);
+        console.log("created");
+        
     }
+    
   } catch (error) {
     console.error('Error creating default admin:', error);
   }
